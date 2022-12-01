0a10d6ca97c3af4ed7243a690e213b5716628fcd
Ryan Christilaw <ryan.christilaw@gmail.com>
2022-12-01 10:19:17 -0500

    Day 1

--- src/main/java/Day01.java
+++ src/main/java/Day01.java
@@ -2,6 +2,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Day01 extends BaseDay {
@@ -15,13 +17,68 @@ public class Day01 extends BaseDay {
 
     public void runPart1() throws URISyntaxException, IOException {
         final var input = readClassPathResource(inputFile);
+        final var calorieReadings = input.lines()
+            .map(it -> {
+                try {
+                    return Integer.valueOf(it);
+                } catch (Exception exception) {
+                    return null;
+                }
+            })
+            .toList();
 
-        log.info("Results");
+        final var sums = new ArrayList<Integer>();
+
+        var currentSum = 0;
+        for (final var reading : calorieReadings) {
+            if (reading == null) {
+                sums.add(currentSum);
+                currentSum = 0;
+                continue;
+            }
+            currentSum += reading;
+        }
+
+        final var orderedSums = sums.stream()
+            .sorted()
+            .toList();
+
+        log.info("Results: {}", orderedSums);
     }
 
     public void runPart2() throws URISyntaxException, IOException {
         final var input = readClassPathResource(inputFile);
+        final var calorieReadings = input.lines()
+            .map(it -> {
+                try {
+                    return Integer.valueOf(it);
+                } catch (Exception exception) {
+                    return null;
+                }
+            })
+            .toList();
 
-        log.info("Results");
+        final var sums = new ArrayList<Integer>();
+
+        var currentSum = 0;
+        for (final var reading : calorieReadings) {
+            if (reading == null) {
+                sums.add(currentSum);
+                currentSum = 0;
+                continue;
+            }
+            currentSum += reading;
+        }
+
+        final var orderedMonkeys = sums.stream()
+            .sorted()
+            .toList();
+
+        final var numberOfMonkeys = orderedMonkeys.size();
+        final var topMonkeys = orderedMonkeys.get(numberOfMonkeys - 1)
+            + orderedMonkeys.get(numberOfMonkeys - 2)
+            + orderedMonkeys.get(numberOfMonkeys - 3);
+
+        log.info("Results: {}", topMonkeys);
     }
 }
